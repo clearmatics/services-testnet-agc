969f548d40861a28e02b78b07699ad4318e422d7
ben-clearmatics <59572435+ben-clearmatics@users.noreply.github.com>
2021-01-12 09:45:51 +0000

    Update wait-for-fluentd.sh

--- secrets/wait-for-fluentd.sh
+++ secrets/wait-for-fluentd.sh
@@ -4,32 +4,44 @@ host=$1
 shift
 cmd=$@
 
-recurse(){
+foo(){
     HEADERS=`curl -Is --connect-timeout 5 $host`
     CURLSTATUS=$?
     HTTPSTATUS=`echo $HEADERS | grep HTTP | cut -d' ' -f2`
 
-    if [ $CURLSTATUS -eq 28 ]
-        then
-            echo "fluentd not ready yet"
-            sleep 4s
-            recurse
+    # Check curl status
+    while [ $CURLSTATUS -eq 28 ]
+    do
+        echo "fluentd not ready yet"
+        sleep 4s
+        HEADERS=`curl -Is --connect-timeout 5 $host`
+        CURLSTATUS=$?
+    done
 
-    else
-        # Check HTTP status code
-        if [ $HTTPSTATUS -eq 200 ]
-            then
-                echo "Alright, fluentd is ready"
-                echo "http status = $HTTPSTATUS"
-                # echo $cmd
-                exec $cmd
-        else
-            echo "fluentd not ready yet"
-            sleep 4s
-            recurse
-        fi
-    fi
+    # check HTTP status is not empty
+    while [ -z "$HTTPSTATUS" ]
+    do
+        echo "fluentd not ready yet"
+        sleep 4s
+        HEADERS=`curl -Is --connect-timeout 5 $host`
+        HTTPSTATUS=`echo $HEADERS | grep HTTP | cut -d' ' -f2`
+    done
+
+    # Check HTTP status code
+    while [ $HTTPSTATUS -ne 200 ]
+    do
+        echo "fluentd not ready yet"
+        sleep 4s
+        HEADERS=`curl -Is --connect-timeout 5 $host`
+        HTTPSTATUS=`echo $HEADERS | grep HTTP | cut -d' ' -f2`
+    done
+
+
+    echo "Alright, fluentd is ready"
+    echo "http status = $HTTPSTATUS"
+    # echo $cmd
+    exec $cmd
+
     } 
 
-recurse
-
+foo
